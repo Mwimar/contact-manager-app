d88012e7b4d2730e9ce0466fa476434d212ac27e
Mark Mwima <markmwimar@gmail.com>
2023-10-02 19:20:29 +0300

    handling form data

--- Desktop/nodejs/app.js
+++ Desktop/nodejs/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 
 const app = express();
+ 
+app.use(express.urlencoded({ extended: false }));//urlencoded checks requests for form data
 
 app.get('/currenttime', function (req, res) {
     res.send('<h1>' + new Date().toISOString() + '<h1/>')}
 );
 
 app.get('/', function (req, res) {
-    res.send('<h1>Karibu Huku OG</h1>');
+    res.send('<form action="/store-user" method="POST"><label>Your Name<label> <input type="text" name="username"> <button>Submit</button></form>');
 })//localhost 3000/currenttime;
+
+app.post('/store-user', function (req, res) {
+    const userName = req.body.username; //extract username
+    console.log(userName);
+    res.send('<h1>User Name Received</h1>')
+})
 
 app.listen(3000);
 
@@ -25,4 +33,4 @@ app.listen(3000);
 // const server = http.createServer(handleRequest);
 // server.listen(3000); //listening incoming requests on port 3000 for development purposes
 
-// switch to port 80 0r 443 on an actual server
+// switch to port 80 0r 443 on an actual server 
