c207751cf4e86126f60e4696e28931833c27d945
Mark Mwima <markmwimar@gmail.com>
2023-10-02 18:21:48 +0300

    express intro

--- Desktop/nodejs/app.js
+++ Desktop/nodejs/app.js
@@ -1,15 +1,28 @@
-const http = require('http');
-function handleRequest(request, response) {
-    if (request.url === '/currenttime') {
-        response.statusCode = 200;
-    response.end('<h1>' +new Date().toISOString()+ '<h1/>')
+const express = require('express');
+
+const app = express();
+
+app.get('/currenttime', function (req, res) {
+    res.send('<h1>' + new Date().toISOString() + '<h1/>')}
+);
+
+app.get('/', function (req, res) {
+    res.send('<h1>Karibu Huku OG</h1>');
+})//localhost 3000/currenttime;
+
+app.listen(3000);
+
+// function handleRequest(request, response) {
+//     if (request.url === '/currenttime') {
+//         response.statusCode = 200;
+//     response.end('<h1>' +new Date().toISOString()+ '<h1/>')
             
-    } else if (request.url === '/') {
-        response.statusCode =200;//200 to indicate success 404 vice-versa
-        response.end('<h1>Karibu Huku OG</h1>')        
-    }
-};
-const server = http.createServer(handleRequest);
-server.listen(3000); //listening incoming requests on port 3000 for development purposes
+//     } else if (request.url === '/') {
+//         response.statusCode =200;//200 to indicate success 404 vice-versa
+//         response.end('<h1>Karibu Huku OG</h1>')        
+//     }
+// };
+// const server = http.createServer(handleRequest);
+// server.listen(3000); //listening incoming requests on port 3000 for development purposes
 
 // switch to port 80 0r 443 on an actual server
