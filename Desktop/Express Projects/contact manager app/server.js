e076a2f16153e4c3a9d95046c9ea097754caae66
Mark Mwima <markmwimar@gmail.com>
2024-04-20 19:13:47 +0300

    json response

--- Desktop/Express Projects/contact manager app/server.js
+++ Desktop/Express Projects/contact manager app/server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "get all contacts" });
+});
+
 app.listen(port, () => {
   console.log("Were Up Baby!!!!!!!");
 });
