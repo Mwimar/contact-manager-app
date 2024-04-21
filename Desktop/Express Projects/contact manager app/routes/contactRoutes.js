c71f7d102ffb94b9fbad19bb458889064296518a
Mark Mwima <markmwimar@gmail.com>
2024-04-21 10:47:08 +0300

    additional routes

--- Desktop/Express Projects/contact manager app/routes/contactRoutes.js
+++ Desktop/Express Projects/contact manager app/routes/contactRoutes.js
@@ -6,4 +6,20 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "get all contacts" });
 });
 
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Create contacts" });
+});
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+});
+
 module.exports = router;
