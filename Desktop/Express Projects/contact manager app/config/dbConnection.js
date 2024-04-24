09b36b75348b867404f7dac09adfeac28a994eb4
Mark Mwima <markmwimar@gmail.com>
2024-04-24 20:01:37 +0300

    contact model schema

--- Desktop/Express Projects/contact manager app/config/dbConnection.js
+++ Desktop/Express Projects/contact manager app/config/dbConnection.js
@@ -1,8 +1,8 @@
-const mongose = require("mongoose");
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const connect = await mongose.connect(process.env.CONNECTION_STRING);
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
     console.log(
       "Database Connected:",
       connect.connection.host,
