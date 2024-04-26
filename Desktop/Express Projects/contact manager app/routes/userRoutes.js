fc39456962a2481a71677508863edcb3692065d2
Mark Mwima <markmwimar@gmail.com>
2024-04-27 00:29:10 +0300

    login and current routes

--- Desktop/Express Projects/contact manager app/routes/userRoutes.js
+++ Desktop/Express Projects/contact manager app/routes/userRoutes.js
@@ -4,3 +4,11 @@ const router = express.Router();
 router.post("/register", (req, res) => {
   res.json({ message: "Register the User" });
 });
+
+router.post("/login", (req, res) => {
+  res.json({ message: "log in User" });
+});
+
+router.post("/current", (req, res) => {
+  res.json({ message: "Current User information" });
+});
