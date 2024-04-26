9a12d21c0cd97b7856496e686f07d7a83cb234de
Mark Mwima <markmwimar@gmail.com>
2024-04-27 01:32:29 +0300

    hashing user password

--- Desktop/Express Projects/contact manager app/controllers/userController.js
+++ Desktop/Express Projects/contact manager app/controllers/userController.js
@@ -1,9 +1,29 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@description Register User;
 //path api/user/register
 //access Public
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are Mandatory");
+  }
+
+  const userAvailable = await User.findOne({ email });
+
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //hashed password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password:", hashedPassword);
+
   res.status(200).json({ message: "Register the User" });
 });
 
