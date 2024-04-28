90d6eb1f021a5092d15796f2d955457756fae33f
Mark Mwima <markmwimar@gmail.com>
2024-04-28 22:38:58 +0300

    validating users

--- Desktop/Express Projects/contact manager app/middleware/validateTokenHandler.js
+++ Desktop/Express Projects/contact manager app/middleware/validateTokenHandler.js
@@ -1,45 +1,45 @@
-// const asyncHandler = require("express-async-handler");
-// const jwt = require("jsonwebtoken");
-
-// const validateToken = asyncHandler(async (req, res, next) => {
-//   let token;
-//   let authHeader = req.headers.Authorization || req.headers.Authorization;
-//   if (authHeader && authHeader.startsWith("Bearer")) {
-//     token = authHeader.split(" ")[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//       if (err) {
-//         res.status(401);
-//         throw new Error("User not Authorized");
-//       }
-//       console.log(decoded);
-//     });
-//   }
-// });
-
-// module.exports = { validateToken };
-
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
-  let authHeader = req.headers.authorization; // Use lowercase 'authorization'
-
+  let authHeader = req.headers.authorization || req.headers.authorization;
   if (authHeader && authHeader.startsWith("Bearer ")) {
-    // Added space after Bearer
     token = authHeader.split(" ")[1];
-
-    try {
-      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error("User not Authorized");
+      }
       console.log(decoded);
-      req.user = decoded; // Store the decoded token to req.user
-      next(); // Proceed to the next middleware or route handler
-    } catch (err) {
-      res.status(401).json({ message: "User not authorized" });
-    }
-  } else {
-    res.status(401).json({ message: "No token provided, user not authorized" });
+    });
   }
 });
 
 module.exports = { validateToken };
+
+// const asyncHandler = require("express-async-handler");
+// const jwt = require("jsonwebtoken");
+
+// const validateToken = asyncHandler(async (req, res, next) => {
+//   let token;
+//   let authHeader = req.headers.authorization; // Use lowercase 'authorization'
+
+//   if (authHeader && authHeader.startsWith("Bearer ")) {
+//     // Added space after Bearer
+//     token = authHeader.split(" ")[1];
+
+//     try {
+//       const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+//       console.log(decoded);
+//       req.user = decoded; // Store the decoded token to req.user
+//       next(); // Proceed to the next middleware or route handler
+//     } catch (err) {
+//       res.status(401).json({ message: "User not authorized" });
+//     }
+//   } else {
+//     res.status(401).json({ message: "No token provided, user not authorized" });
+//   }
+// });
+
+// module.exports = { validateToken };
